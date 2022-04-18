8983a1a4c30aa75910fc128a07a8595a9ddb1d4a
carlos rodriguez <twincarlos98@gmail.com>
2022-04-18 19:11:09 -0400

    finished some styling and added the player

--- react-app/src/components/ContentPage/index.js
+++ react-app/src/components/ContentPage/index.js
@@ -3,6 +3,7 @@ import './ContentPage.css';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 import NavBar from '../NavBar';
 import { getOneContent } from '../../store/content';
@@ -13,6 +14,11 @@ function ContentPage () {
     const contentId = useParams().contentId;
     const content = useSelector(state => state.content.content);
 
+    const [playing, setPlaying] = useState(false);
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [volume, setVolume] = useState(1);
+
     useEffect(() => {
         (async () => await dispatch(getOneContent(contentId)).then(() => setLoaded(true)))();
     }, [dispatch, contentId]);
@@ -21,13 +27,39 @@ function ContentPage () {
 
     return (
         <div id='content-background' style={{ backgroundImage: `url('${content.background_picture}')` }}>
-            <NavBar backgroundColor={'transparent'} />
+            { !width && <NavBar backgroundColor={'transparent'} /> }
+            <ReactPlayer
+                url={content.media}
+                width={width + "%"}
+                height={height + "%"}
+                playing={playing}
+                onPause={() => setPlaying(false)}
+                onPlay={() => setPlaying(true)}
+                volume={volume}
+            />
+            {
+                width ? (
+                <div id='controls'>
+                    { playing && <button onClick={() => setPlaying(false)}>Pause</button> }
+                    { !playing && <button onClick={() => setPlaying(true)}>Play</button> }
+                    <input onChange={e => setVolume(e.target.value)} type='range' min={0} max={1} step={0.01} value={volume}></input>
+                    <button onClick={() => {
+                        setPlaying(false);
+                        setWidth(0);
+                        setHeight(0);
+                    }}>Fullscreen</button>
+                </div>
+            ) : null}
             <div id='content-page'>
                 <div id='content-details'>
                     <img style={{ width: '15vw', marginBottom: 20 }} src={content.logo} alt=''></img>
                     <h1 style={{ marginBottom: 10, fontSize: 80 }}>{content.title}</h1>
                     <span style={{ marginBottom: 30 }}>
-                        <button id='play'><i className="fas fa-play"></i> Play</button>
+                        <button id='play' onClick={() => {
+                            setWidth(100);
+                            setHeight(100);
+                            setPlaying(true);
+                        }}><i className="fas fa-play"></i> Play</button>
                         <button id='fav'><i className="fas fa-plus"></i></button>
                     </span>
                     <p>{content.description}</p>
